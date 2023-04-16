8d5aabb869f0dbee85323eca325efc2d23ca7093
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-17 00:59:53 +0200

    Tests

---
+++ core.asm
@@ -0,0 +1,44 @@
+global core
+
+section .text
+
+global .start
+    xor rdx, rdx
+.main_loop:
+    mov     al, byte [rsi + rdx + 0]
+    cmp     al, 0x0
+    je      .end
+    cmp     al, '0'
+    jl      .not_number
+    cmp     al, '9'
+    jg      .not_number
+    jmp     .operation_number
+.not_number:
+    cmp     al, 'E'
+    je      .operation_E
+    cmp     al, 'D'
+    je      .operation_D
+    cmp     al, 'E'
+    je      .operation_E
+    inc     rdx
+    jmp     .main_loop
+.operation_number:
+    sub     al, '0'
+    push    al
+    jmp     .main_loop
+.operation_C:
+    sub     esp, 1
+    jmp     .main_loop
+.operation_D:
+    pop     al
+    push    al
+    push    al
+    jmp     .main_loop
+.operation_E:
+    pop     al
+    pop     cl
+    push    al
+    push    cl
+    jmp     .main_loop
+.end:
+    ret
