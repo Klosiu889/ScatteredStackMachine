f3a3db06ddd60abcb668195f732749eaa15c46e4
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-18 01:00:56 +0200

    Improvements and bug fixes.

--- core.asm
+++ core.asm
@@ -77,7 +77,8 @@ core:
     jmp         .main_loop
 .operation_G:
     call        get_value
-    push        edi
+    mov         eax, edi
+    ret
     jmp         .main_loop
 .end:
     pop         rax
