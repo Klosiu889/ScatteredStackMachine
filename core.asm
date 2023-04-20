6a569612a4135650cafb2684a68b546e24b68c76
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-20 23:06:11 +0200

    Improvements and bug fixes.

--- core.asm
+++ core.asm
@@ -23,7 +23,7 @@ core:
 .main_loop:
     mov         al, byte [r13 + rbx + 0]
     inc         rbx
-    cmp         r12, 0x7
+    cmp         r12, 0x8
     jnz         .continue
     mov         rdx, [rsp]
     push        rax
