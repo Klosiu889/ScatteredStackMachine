78e7d26feef44ec28fc05aefe6dfd1960fbf7828
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-20 23:22:12 +0200

    Improvements and bug fixes.

--- core.asm
+++ core.asm
@@ -24,7 +24,7 @@ core:
     cmp         r12, 0x1
     jnz         .continue
     mov         rdi, r12
-    mov         rsi, byte [r13 + rbx + 0]
+    mov         rsi, qword [r13 + rbx + 0]
     mov         rdx, [rsp]
     call        print_register
     mov         al, byte [r13 + rbx + 0]
